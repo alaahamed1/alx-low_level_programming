60e75ae2078f1a9fd4eb4df1d3c47afad69f09e7
alaahamed800 <alaa.elzeftawy@gmail.com>
2023-12-14 14:49:58 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - small function prints a line 
+ *  Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
