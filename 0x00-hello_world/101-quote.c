48fb19b35aafaa9379131ff5d6b2e166eff9ddff
alaahamed800 <alaa.elzeftawy@gmail.com>
2023-12-15 01:48:49 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print a line to the standard error
+ * return 1 (success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r" , stdout);
+return (1);
+}
