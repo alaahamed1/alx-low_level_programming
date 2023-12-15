da2f58a60a6312544355e1abbb1de2312c32124f
alaahamed800 <alaa.elzeftawy@gmail.com>
2023-12-15 01:51:31 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - print a line to the standard error
- * return 1 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r" , stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
