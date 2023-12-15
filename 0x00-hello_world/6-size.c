99ae4711045814d8842ea69cfd4d3391a20008ba
alaahamed800 <alaa.elzeftawy@gmail.com>
2023-12-15 01:04:20 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,11 @@
  * return: 0 (success)
  * */
 int main(void)
-}
-    printf("Size of a char: %lu byte(s)\n", (unsigned log) sizeof(char));
-    
- printf("Size of a char: %lu byte(s)\n", (unsigned log) sizeof(char));
-printf("Size of a char: %lu byte(s)\n", (unsigned log) sizeof(int));
-printf("Size of a char: %lu byte(s)\n", (unsigned log) sizeof(long int));
-printf("Size of a char: %lu byte(s)\n", (unsigned log) sizeof(long long int));
-printf("Size of a char: %lu byte(s)\n", (unsigned log) sizeof(float));
+       	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+   	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(long int));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(float));
 return(0);
 }
