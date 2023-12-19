a87e95acd3bd456b630c3ee26fa872c037fe98c2
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-19 04:40:00 +0200

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return:return 0
+*/
+
+int main(void)
+{
+char i;
+
+for (i = 'z'; i >= 'a'; i--)
+
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
