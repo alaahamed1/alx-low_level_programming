dfd884809a3786fe2123b47e210c3b671c7550b3
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-19 04:35:28 +0200

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point
+* Return:return 0 (success)
+*/
+
+int main(void)
+{
+int x;
+
+while (x < 10)
+{
+putchar(x + '0');
+x++;
+}
+putchar('\n');
+return (0);
+}
