d24458f88582ef9a034bca0cb0f654d594c472af
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-19 04:25:01 +0200

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return:return 0 (Success)
+*/
+int main(void)
+{
+char i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+
+putchar('\n');
+return (0);
+}
