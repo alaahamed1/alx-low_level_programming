e84a144ef8e6007a46150cc9fe17aceb6039bb8a
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-19 03:40:06 +0200

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function 
+ * Return: 0
+ */
+
+int main(void)
+{
+    char i;
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        putchar(i);
+        putchar('\n');
+    }
+    return (0);
+}
