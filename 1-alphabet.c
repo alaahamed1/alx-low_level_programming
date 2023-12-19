128a49c13dcce74930d29549412501e7b8b6a8da
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-19 22:05:39 +0200

    task 1

---
+++ 1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - doning alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+
+_putchar('\n');
+}
