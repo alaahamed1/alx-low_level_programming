b7d78742b999ef6606e73628239b164a91eadc96
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-20 14:30:56 +0200

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+_putchar(43);
 return (1);
 }
 else if (n == 0)
 {
-putchar('0');
+_putchar(45);
 return (0);
 }
 else
 {
-putchar('-');
+_putchar(48);
 return (-1);
 }
 }
