35b30e032221ff21493fd992def0e204db209a3a
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-20 22:05:50 +0200

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_to_98 - print a num to 98
+ * @n: The number to start printing
+ * Return: Always 0.
+ */
+
 void print_to_98(int n)
 {
 if (n <= 98)
