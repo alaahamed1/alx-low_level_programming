3da254fa69f30f48c74e55f5995a77d579ca90cc
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-20 16:10:14 +0200

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ */
+void jack_bauer(void)
+{
+    int H = 0, M = 0;
+
+    while (H < 24)
+    {
+        M = 0;
+        while (M < 60)
+        {
+            _putchar((H / 10) + '0');
+            _putchar((H % 10) + '0');
+            _putchar(58);
+            _putchar((M / 10) + '0');
+            _putchar((M % 10) + '0');
+            _putchar('\n');
+            M++;
+        }
+        H++;
+    }
+}
