22180975653de2706bab86abb10f5c3c1cd0574c
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-20 23:48:07 +0200

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
-int i = 0;
-long long a = 1, b = 2, sum = b;
 
-while (b + a < 4000000) {
+/**
+ * main - the main printing funtion
+ * less than 4000000.
+ * Return: 0.
+ */
+
+
+int main(void)
+{
+int i = 0;
+long a = 1, b = 2, sum = b;
+
+
+while (b + a < 4000000)
+{
 b += a;
-if (b % 2 == 0) {
-    sum += b;
-}
+if (b % 2 == 0)
+sum += b;
 a = b - a;
 ++i;
 }
-printf("%lld\n", sum);
-return 0;
+printf("%ld", sum);
+return (0);
 }
