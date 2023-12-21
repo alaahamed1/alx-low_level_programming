5cfe25fe98d3030d9a479d7f3e41f295ee28094a
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-21 22:14:40 +0200

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,10 +12,14 @@ if (n <= 0)
 _putchar('\n');
 return;
 }
-while (n > 0)
+
+for (int i = 0; i < n; i++)
 {
+for (int j = 0; j < i; j++)
+{
+_putchar(' ');
+}
 _putchar('\\');
 _putchar('\n');
-n--;
 }
 }
