3a76daaf995db28511e2249cadb85f8aa3f8d7b7
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-21 22:29:44 +0200

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,17 @@
 
 void print_square(int size)
 {
+int a = 0;
+int b = 0;
+
 if (size <= 0)
 {
 _putchar('\n');
 return;
 }
-int a = 0;
 while (a < size)
 {
-int b = 0;
+b = 0;
 while (b < size)
 {
 _putchar('#');
