64cac443ee6a80449d4684fab99c782a28667cbe
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-22 15:19:52 +0200

    task 12

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,14 +10,14 @@ int main(void)
 {
 int i;
 int sum = 0;
-
 for (i = 3; i < 1024; i = i + 3)
 {
 sum += i;
 }
+
 for (i = 5; i < 1024; i = i + 5)
 {
-if (i % 3 != 0) // To avoid double counting multiples of 15
+if (i % 3 != 0)
 {
 sum += i;
 }
