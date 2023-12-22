166c5c9b892f34eac0350450eb88498f952583df
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-22 20:41:01 +0200

    Task11 in 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * this code prints all possible combinations of single-digit numbers.
+ * followed by a new line.
+ * It is not allowed to use any variable of type char
+ * only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * only use putchar four times max in your code
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
 int n1, n2, n3;
