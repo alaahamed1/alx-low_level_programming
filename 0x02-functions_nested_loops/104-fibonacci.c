600c03202b2e9dd7aea32544c4b02545f3625234
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-22 15:26:16 +0200

    task 14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
+#include "main.h"
 
 int main(void) {
-unsigned long a = 1, b = 2, nextTerm;
-int i = 3;
+unsigned long long max = 1000000000000000ULL;
+unsigned long long prev1 = 0, curr1 = 0, sum1 = 0;
+unsigned long long prev2 = 1, curr2 = 2, sum2 = 0;
+int fib_num = 98;
+unsigned long long fib_sequence[fib_num];
+int i;
 
-printf("%lu, %lu, ", a, b);
+fib_sequence[0] = prev2;
+fib_sequence[1] = curr2;
 
-for (; i <= 98; ++i) {
-nextTerm = a + b;
-printf("%lu", nextTerm);
+for (i = 2; i < fib_num; i++) {
+sum2 = curr2 + prev2;
+sum1 = (sum2 / max) + curr1 + prev1;
+sum2 = sum2 % max;
 
-if (i != 98) {
+prev1 = curr1;
+prev2 = curr2;
+curr1 = sum1;
+curr2 = sum2;
+
+if (curr1 != 0) {
+fib_sequence[i] = curr1 * max + curr2;
+} else {
+fib_sequence[i] = curr2;
+}
+}
+
+for (i = 0; i < fib_num; i++) {
+printf("%llu", fib_sequence[i]);
+if (i < fib_num - 1) {
 printf(", ");
 }
-
-a = b;
-b = nextTerm;
 }
-
 printf("\n");
+
 return 0;
 }
