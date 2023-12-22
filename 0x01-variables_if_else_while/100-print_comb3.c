3a6e59ec237d783444adf0d3247d85179510959c
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-22 20:09:19 +0200

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-int main(void) {
-int n;
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
 
-for (n = 48; n <= 57; n++)
+int main(void) {
+int n1, n2;
+
+for (n1 = 0; n1 < 9; n1++)
 {
-putchar(n);
-if (n != 57)
+for (n2 = n1 + 1; n2 <= 9; n2++)
+{
+putchar(n1 + '0');
+putchar(n2 + '0');
+
+if (!(n1 == 8 && n2 == 9))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 
