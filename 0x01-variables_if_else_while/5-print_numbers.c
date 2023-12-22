d554f41e4b3d69056652f4b9729cb727d9142dd4
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-22 19:46:49 +0200

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* Return:return 0 (Success)
-*/
+ * main - Entry point
+ *
+ * this code prints the alphabet in lowercase, followed by a new line.
+ * Print all the letters except q and e
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-char i;
-
-for (i = '0'; i <= '9'; i++)
+for (char i = '0'; i <= '9'; i++)
 {
 putchar(i);
 }
 
 putchar('\n');
-return (0);
+return 0;
 }
+
