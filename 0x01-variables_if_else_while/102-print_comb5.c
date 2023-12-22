4b970fe597f13a29f841878902b19f0233f98ed2
alaahamed1 <alaa.elzeftawy@gmail.com>
2023-12-22 20:51:00 +0200

    Task12 in 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+int n1, n2;
+
+for (n1 = 0; n1 <= 99; n1++)
+{
+for (n2 = n1 + 1; n2 <= 99; n2++)
+{
+if (n1 < 10)
+{
+putchar('0');
+putchar(n1 + '0');
+}
+else
+{
+putchar((n1 / 10) + '0');
+putchar((n1 % 10) + '0');
+}
+putchar(' ');
+if (n2 < 10)
+{
+putchar('0');
+putchar(n2 + '0');
+}
+else
+{
+putchar((n2 / 10) + '0');
+putchar((n2 % 10) + '0');
+}
+
+if (!(n1 == 98 && n2 == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
