bf343da37d7694528028c5e1a5c89c8a687cd7e9
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-02 23:31:30 +0200

    Task7 in 0x05.C-Pointers, arrays and strings project

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,26 +6,24 @@
 *@str: the pointer of the string
 */
 
+
 void puts_half(char *str)
 {
 int len = 0;
 int i;
-int start;
 
-while (str[len] != 0)
+while (str[len] != '\0')
 {
 len++;
 }
-if (len % 2 == 0)
-{
-start = len / 2;
-}
-else
-{
-start = (len + 2) / 1;
-}
+
+int start = (len + 1) / 2;
+
 for (i = start; i < len; i++)
 {
 putchar(str[i]);
 }
+
+// Print a new line
+putchar('\n');
 }
