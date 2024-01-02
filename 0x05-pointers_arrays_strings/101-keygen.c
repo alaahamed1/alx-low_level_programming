b71c911afb721bb13f3959bfb9a498f266cbc5f4
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-03 00:31:58 +0200

    Task11 in 0x05.C-Pointers, arrays and strings project

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,2 +1,41 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+char* generate_password()
+{
+static const char CHARSET[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+char* password = (char*)malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+
+if (password == NULL)
+{
+fprintf(stderr, "Memory allocation failed\n");
+exit(EXIT_FAILURE);
+}
+
+srand(time(NULL));
+
+for (int i = 0; i < PASSWORD_LENGTH; ++i)
+{
+int index = rand() % (sizeof(CHARSET) - 1);
+password[i] = CHARSET[index];
+}
+
+password[PASSWORD_LENGTH] = '\0';
+return password;
+}
+
+void print_password(char* password)
+{
+printf("Generated password: %s\n", password);
+}
+
+int main()
+{
+char* password = generate_password();
+print_password(password);
+free(password);
+return 0;
+}
