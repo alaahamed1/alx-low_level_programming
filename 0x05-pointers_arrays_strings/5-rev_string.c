0f78bfb05982ac15ebf3aecb41e5e2cb9a8c0bf7
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-02 21:14:56 +0200

    Task5 in 0x05.C-Pointers, arrays and strings project

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,11 @@ void rev_string(char *s)
 {
 int length = 0;
 char temp;
-
+int i;
 while (s[length] != '\0')
 length++;
 
-for (int i = 0; i < length / 2; i++)
+for (i = 0; i < length / 2; i++)
 {
 temp = s[i];
 s[i] = s[length - i - 1];
