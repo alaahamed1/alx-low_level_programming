05b458370aa908b488e243531a79afeb0afa9d5f
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-02 19:38:14 +0200

    Task3 in 0x05.C-Pointers, arrays and strings project

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _puts - prints a string, followed by a new line, to stdout
+* @str: the pointer of the string
+*/
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
