15e1c9d02d3fed3b013c02385fdc1e43de8a78d0
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-03 00:08:36 +0200

    Task9 in 0x05.C-Pointers, arrays and strings project

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: the buffer to copy to
+ * @src: the string to copy from
+ * Return: pointer of the new dest
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+if (src == NULL || dest == NULL)
+{
+return NULL;
+}
+
+char *newDest = dest;
+
+while ((*dest++ = *src++) != '\0');
+
+return newDest;
+}
