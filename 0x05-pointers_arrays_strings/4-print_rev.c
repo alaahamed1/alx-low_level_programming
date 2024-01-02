b5a40819d1867d46687522fc3802755fb3e5cb72
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-02 20:46:36 +0200

    Task4 in 0x05.C-Pointers, arrays and strings project

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,14 @@
 void print_rev(char *s)
 {
 int length = 0;
+int i;
 
 while (s[length] != '\0')
 {
 length++;
 }
 
-for (int i = length - 1; i >= 0; i--)
+for (i = length - 1; i >= 0; i--)
 {
 putchar(s[i]);
 }
