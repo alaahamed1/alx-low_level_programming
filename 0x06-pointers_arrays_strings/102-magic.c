a22da4a5be5fba69ded4a2c6cad6f07a5baddd3c
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-04 23:38:37 +0200

    Advanced Task-10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,17 +6,17 @@
  * Return: 0
  */
 
-#include <stdio.h>
 
 int main(void)
 {
 int n;
 int a[5];
 int *p;
+
 a[2] = 1024;
 p = &n;
-/* Refactored line */
-*(p + (sizeof(int) * 5)) = 98;
+/* Add the line below */
+*(p + 5) = 98;
 
 printf("a[2] = %d\n", a[2]);
 return (0);
