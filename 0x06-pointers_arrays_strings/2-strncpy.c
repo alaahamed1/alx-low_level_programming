a0fe7ac82d4b7681cd6780c3650d80080ec6f9dc
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-04 20:08:20 +0200

    According to betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,9 +4,9 @@
  * _strncat - concatenates two strings
  * @dest: first string
  * @src: second string
- * @n: the max number of bytes to print from src
+ * @n: the max number of bytes to concatenate from src
  * Return: pointer to the string dest
-*/
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
