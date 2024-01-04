2e1faca55b3dbacaeaea8db1e1132b46ac101e86
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-05 00:11:37 +0200

    Advanced Task-9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,10 +4,11 @@
 /**
  * print_number - Prints an integer using _putchar
  * @n: The number to print
- * Description: This function prints an integer using only the _putchar function.
- *It does not use long, arrays, pointers, or hard-coded special values.
- *If the number is negative, it starts by printing the negative sign '-'
- *followed by the absolute value of the number.
+ *
+ * Description: Prints the integer 'n' using the _putchar function.
+ *              Restrictions: It does not use long, arrays, pointers, or hard-coded special values.
+ *              If the number is negative, it starts by printing the negative sign '-'
+ *              followed by the absolute value of the number.
  */
 
 void print_number(int n)
