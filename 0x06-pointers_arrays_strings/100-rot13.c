fe857963c788ffab1dfb50e396b3cea8ffd81ce0
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-05 00:30:36 +0200

    Advanced Task-8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * rot13 - Encodes a string using rot13.
- * @s: Pointer to the string to be encoded.
- * Return: Pointer to the encoded string.
- */
-
 char *rot13(char *s)
 {
 int i;
-char *ptr = s;
 
-while (*s)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (i = 0; i < 52; i++)
+char c = s[i];
+
+if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 {
-if ((*s >= 'a' && *s <= 'z' && *s == 'a' + (i % 26)) ||
-(*s >= 'A' && *s <= 'Z' && *s == 'A' + (i % 26)))
+if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
 {
-*s = (*s <= 'Z') ? 'A' + (i + 13) % 26 : 'a' + (i + 13) % 26;
-break;
+s[i] += 13;
+}
+else
+{
+s[i] -= 13;
 }
 }
-s++;
 }
 
-return (ptr);
+return (s);
 }
