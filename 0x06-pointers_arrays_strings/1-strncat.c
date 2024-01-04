e20339b93686c531b94c4c5d96565303336477dc
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-04 18:50:52 +0200

    Task1 in 0x06.C-More pointers, arrays and strings project

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ * it will use at most n bytes from src
+ * src does not need to be null-terminated if it contains n or more bytes
+ * @dest: the first string to append
+ * @src: the second string
+ * @n: the number of bytes to print from src
+ * Return a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, const char *src, int n)
+{
+int i = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+
+int j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+dest[i] = '\0';
+
+return dest;
+}
