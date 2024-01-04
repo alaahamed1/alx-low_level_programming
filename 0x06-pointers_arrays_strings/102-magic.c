babcff3f68fb6d271fbe3befbeb28047caa80e27
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-04 23:27:29 +0200

    Advanced Task-10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,6 +6,14 @@ int main(void)
 int a[] = {0, 1, 2, 3, 4};
 int *p;
 
+/*
+* write your line of code here
+*you are not allowed to use a
+*you are not allowed to modify p
+*only one statement
+*you are not allowed to code anything else than this line of code
+*/
+
 p = &a[0];
 *(p + 2) = 98;
 
