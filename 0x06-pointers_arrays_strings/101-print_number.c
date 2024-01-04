72ace58287dac5cec88d79ef8563e7e9b3a9b301
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-04 23:10:45 +0200

    Modify the code according to betty rules

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,7 +14,8 @@ void print_number(int n)
 {
 int divisor = 1;
 
-if (n < 0) {
+if (n < 0)
+{
 _putchar('-');
 n = -n;
 }
@@ -26,7 +27,7 @@ divisor *= 10;
 
 while (divisor != 0)
 {
-_putchar((n / divisor) + '0');
+_putchar((n / divisor) +'0');
 n %= divisor;
 divisor /= 10;
 }
