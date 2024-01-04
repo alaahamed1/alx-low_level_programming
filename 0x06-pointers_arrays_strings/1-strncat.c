d4a0b43a0bd3c31c2593859871f101654894cba9
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-04 18:55:16 +0200

    Task1 in 0x06.C-More pointers, arrays and strings project

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,13 +13,13 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
+int j = 0;
 
 while (dest[i] != '\0')
 {
 i++;
 }
 
-int j = 0;
 while (j < n && src[j] != '\0')
 {
 dest[i] = src[j];
