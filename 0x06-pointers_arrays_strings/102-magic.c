e6fe099559d09600cd417d81b3c4ca51f1a07ba0
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-04 23:33:49 +0200

    Advanced Task-10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,24 +6,20 @@
  * Return: 0
  */
 
-int main(void)
-{
-int a[] = {0, 1, 2, 3, 4};
+#include <stdio.h>
+
+int main(void) {
+int n;
+int a[5];
 int *p;
 
-/*
-* write your line of code here
-*you are not allowed to use a
-*you are not allowed to modify p
-*only one statement
-*you are not allowed to code anything else than this line of code
-*/
+a[2] = 1024;
+p = &n;
 
-p = &a[0];
-/*it prints 98\n*/
-*(p + 2) = 98;
+/* Refactored line */
+*(p + (sizeof(int) * 5)) = 98;
 
 printf("a[2] = %d\n", a[2]);
-
 return (0);
 }
+
