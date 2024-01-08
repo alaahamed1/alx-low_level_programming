8b9f923872a7134c1fe9d665977c66cf1645beea
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-09 01:14:01 +0200

    Advanced Task

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,37 +1,26 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * set_string -function that sets the value of a pointer to a char
+ *
  * @s: pointer to a pointer of string
  * @to: pointer to be set
  */
 
 void set_string(char **s, char *to)
 {
-*s = (char *)malloc((strlen(to) + 1) * sizeof(char));
+    free(*s);
 
-if (*s == NULL)
-{
-printf("Memory allocation failed.\n");
-return;
-}
+    *s = (char *)malloc((strlen(to) + 1) * sizeof(char));
 
-strcpy(*s, to);
-}
+    if (*s == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
 
-int main()
-{
-char *str = NULL;
-char *newString = "Hello, there!";
-
-set_string(&str, newString);
-
-if (str != NULL)
-{
-printf("Value of str: %s\n", str);
-free(str);
-}
-
-return 0;
+    strcpy(*s, to);
 }
