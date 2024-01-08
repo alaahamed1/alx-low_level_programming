ce866f8135d205fe902f98406761b595b7f095f0
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-09 01:01:45 +0200

    Task-7 in 0x07.C-Even more pointers, arrays and strings project

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,29 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_diagsums - Prints the sum of the two diagonals
+ * print_diagsums - prints the sum of the two diagonals
  * of a square matrix of integers.
  *
- * @a: Pointer to the first element of the two-dimensional array
- * @size: The size of the square matrix of integers.
+ * @a: pointer the first element of the two-dimensional array
+ * @size: the size of the square matrix of integers.
  */
 void print_diagsums(int *a, int size)
 {
-int prim_diag = 0;
-int sec_diag = 0;
+int prim = 0;
+int sec = 0;
 int i;
 
 for (i = 0; i < size; i++)
 {
-prim_diag += a[i * size + i];
-sec_diag += a[i * size + (size - 1 - i)];
+prim += a[i * size + i];
+sec += a[i * size + (size - 1 - i)];
 }
 
-putchar((prim_diag / 10) + '0');
-putchar((prim_diag % 10) + '0');
-putchar(',');
-putchar(' ');
-putchar((sec_diag / 10) + '0');
-putchar((sec_diag % 10) + '0');
-putchar('\n');
+printf("%d, %d\n", prim, sec);
 }
