49c3feea321a402656f24407c3d04b4938790817
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-08 22:42:46 +0200

    Task-6 in 0x07.C-Even more pointers, arrays and strings project

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,9 +8,11 @@
 
 void print_chessboard(char (*a)[8])
 {
-for (int i = 0; i < 8; i++)
+int i;
+int j;
+for (i = 0; i < 8; i++)
 {
-for (int j = 0; j < 8; j++)
+for (j = 0; j < 8; j++)
 {
 _putchar(a[i][j]);
 }
