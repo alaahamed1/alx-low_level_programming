5aadffe4c78a87b9a808df17bd865b24a700e757
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-11 00:45:17 +0200

    task-1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - a function that prints a string in reverse.
+ * _print_rev_recursion - a function that
+ * prints a string in reverse
  * @s - the string to print
  */
 
