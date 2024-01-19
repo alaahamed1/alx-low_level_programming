8406cd5bce8749f2adba5558819d1918d24eb423
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-19 09:22:27 +0200

    Task-1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main function
+ * @argc:number of arguments passed into the program
+ * @argv:arry arguments
+ * Return: 0 if successful
+ */
 
 int main(int argc , char *argv[])
 {
