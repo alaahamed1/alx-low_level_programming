32eb0f58aef354306972a7d1692e944db29f3e9f
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-19 08:54:46 +0200

    Task-0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /**
- *main - prints its name, followed by a new line
- *@argc: size of the argv array
- *@argv: array of command line arguments
- *Return: 0 (Success)
+ * main - prints its name, followed by a new line
+ * @argc: size of the argv array
+ * @argv: array of command line arguments
+ * Return: 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-		if(argc > 0)
+		if (argc > 0)
 		{
 				printf("%s\n", argv[0]);
 		}
-	return (0);
+		return (0);
 }
