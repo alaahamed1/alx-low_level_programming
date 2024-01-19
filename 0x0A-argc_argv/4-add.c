fcf280c64bd03446032f448e543cc1d3a7e3eb30
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-19 11:06:05 +0200

    Task-4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 
 	for (i = 1; i < argc; i++)
@@ -27,12 +27,12 @@ int main(int argc, char *argv[])
 			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
-				return 1;
+				return (1);
 			}
 		}
 		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
-	return 0;
+	return (0);
 }
