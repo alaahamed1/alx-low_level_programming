c020fd2c0983f23831b33e91f5f7f6aff2da4ab4
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-19 09:44:05 +0200

    Task-2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,12 +8,15 @@
  * Returns: 0
  */
 
+
 int main(int argc, char *argv[])
 {
 	int i;
-	for (i = 1; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
+
 	return (0);
 }
