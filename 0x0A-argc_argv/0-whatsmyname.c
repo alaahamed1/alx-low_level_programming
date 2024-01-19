edad8b597405047ddaced8b14aba4756a870c441
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-19 09:00:56 +0200

    Task-0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-		if (argc > 0)
-		{
-				printf("%s\n", argv[0]);
-		}
-		return (0);
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:returns 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	return (0);
+}
