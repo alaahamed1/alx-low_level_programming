ba0a603269148228e5885931bf6aa0ec3bcbb731
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-20 03:44:01 +0200

    Task-1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,12 +7,12 @@
  */
 unsigned int _strlen(char *s)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (*(s + i) != '\0')
-        i++;
+	while (*(s + i) != '\0')
+		i++;
 
-    return (i);
+	return (i);
 }
 
 /**
@@ -24,32 +24,32 @@ unsigned int _strlen(char *s)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int s1len, s2len, i;
-    char *str;
+	unsigned int s1len, s2len, i;
+	char *str;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    s1len = _strlen(s1);
-    s2len = _strlen(s2);
+	s1len = _strlen(s1);
+	s2len = _strlen(s2);
 
-    if (n > s2len)
-        n = s2len;
+	if (n > s2len)
+		n = s2len;
 
-    str = malloc(s1len + n + 1);
+	str = malloc(s1len + n + 1);
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    for (i = 0; s1[i] != '\0'; i++)
-        str[i] = s1[i];
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
 
-    for (i = 0; i < n; i++)
-        str[i + s1len] = s2[i];
+	for (i = 0; i < n; i++)
+		str[i + s1len] = s2[i];
 
-    str[s1len + n] = '\0';
+	str[s1len + n] = '\0';
 
-    return (str);
+	return (str);
 }
