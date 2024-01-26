d9774a413b17272ddb606359f55ac51fc1e6d414
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-26 04:48:20 +0200

    Task-0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,13 +4,14 @@
  * sum_them_all -a function that returns the sum of all its parameters
  * @n: number of parameters
  * @sum: sum of all parameters
- * Returns: sum of all parameters
+ * Return: sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
+	int i;
 	va_list args;
 	int sum = 0;
+
 	if (n == 0)
 	{
 		return (0);
