b782e827da7835d843eea7741c14ab0fe8edc8ff
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-01-27 00:28:10 +0200

    Task-1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int i;
 	va_list args;
 	va_start(args, n);
-	
+
 	for (i = 0; i < (int)n; i++)
 	{
 		printf("%d", va_arg(args, int));
