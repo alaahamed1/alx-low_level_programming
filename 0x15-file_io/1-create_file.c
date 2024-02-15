0c3e6bcc445190e96426460875d2d132cc9f1954
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-02-15 19:32:27 +0200

    Task0 in 0x15.C-File I/O project and modified README.md

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
 #include "main.h"
 /**
  *_putchar - custom putchar function
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <stdio.h>
+#include "main.h"
 
 int _strlen(char *c)
 {
