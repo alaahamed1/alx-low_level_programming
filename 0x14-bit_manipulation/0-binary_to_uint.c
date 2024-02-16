14951b31fc1d02efef5454091ac7a18a5847eb54
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-02-16 22:31:58 +0200

    Task0 in 0x14.C-Bit manipulation project and created README.md, main.h

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result;
+	unsigned int result = 1;
 
-    if (b == NULL)
-    {
-        return (0);
-    }
-    while (*b != '\0')
-    {
-        if (*b != '0' && *b != '1')
-        {
-            return (0);
-        }
-        result = result * 2 + (*b - '0');
-        b++;
-    }
-    return (result);
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+		result = result * 2 + (*b - '0');
+		b++;
+	}
+	return (result);
 }
