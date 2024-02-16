0921b436330fd95cccf8cbaf3ce0d0ee0d0c5d5f
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-02-16 22:28:29 +0200

    Task0 in 0x14.C-Bit manipulation project and created README.md, main.h

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b:  pointing to a string of 0 and 1 chars
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int result;
+
+    if (b == NULL)
+    {
+        return (0);
+    }
+    while (*b != '\0')
+    {
+        if (*b != '0' && *b != '1')
+        {
+            return (0);
+        }
+        result = result * 2 + (*b - '0');
+        b++;
+    }
+    return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,2 @@
+
+unsigned int binary_to_uint(const char *b);
